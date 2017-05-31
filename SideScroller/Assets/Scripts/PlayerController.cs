55556194d9f2db23661059332dca91f7fe6f1e78
laconictae <danp@datarg.com>
2017-05-31 09:22:58 -0400

    player controller and physics

---
+++ SideScroller/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerController : MonoBehaviour {
+
+    public float gravity = 20;
+    public float speed = 8;
+    public float acceleration = 30;
+    public float jumpHeight = 12;
+
+    private float currentSpeed;
+    private float targetSpeed;
+    public Vector2 amountToMove;
+
+    private PlayerPhysics playerPhysics;
+
+	void Start () {
+        playerPhysics = GetComponent<PlayerPhysics>();
+	}
+	
+	void Update () {
+        targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+        currentSpeed = IncrementTowards(currentSpeed, targetSpeed, acceleration);
+
+        if (playerPhysics.grounded)
+        {
+            amountToMove.y = 0;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                amountToMove.y = jumpHeight;
+            }
+        }
+
+        amountToMove.x = currentSpeed;
+        amountToMove.y -= gravity * Time.deltaTime;
+        playerPhysics.Move(amountToMove * Time.deltaTime);
+	}
+
+    private float IncrementTowards(float n, float target, float a)
+    {
+        if (n == target)
+        {
+            return n;
+        }
+        else
+        {
+            float dir = Mathf.Sign(target - n);
+            n += a * Time.deltaTime * dir;
+            return (dir == Mathf.Sign(target - n)) ? n : target;
+        }
+    }
+}
---
+++ SideScroller/Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerPhysics : MonoBehaviour {
+
+    public LayerMask collisionMask;
+    private BoxCollider collider;
+    private Vector3 s;
+    private Vector3 c;
+
+    private float skin = .005f;
+    [HideInInspector]
+    public bool grounded;
+
+    Ray ray;
+    RaycastHit hit;
+
+    void Start()
+    {
+        collider = GetComponent<BoxCollider>();
+        s = collider.size;
+        c = collider.center;
+    }
+
+	public void Move(Vector2 moveAmount)
+    {
+        float deltaY = moveAmount.y;
+        float deltaX = moveAmount.x;
+        Vector2 p = transform.position;
+
+        grounded = false;
+        for (int i = 0; i < 3; i++)
+        {
+            float direction = Mathf.Sign(deltaY);
+            float x = (p.x + c.x - s.x / 2) + s.x / 2 * i;
+            float y = p.y + c.y + s.y / 2 * direction;
+
+            ray = new Ray(new Vector2(x, y), new Vector2(0, direction));
+            if (Physics.Raycast(ray, out hit, Mathf.Abs(deltaY), collisionMask))
+            {
+                float distance = Vector3.Distance(ray.origin, hit.point);
+
+                if (distance > skin)
+                {
+                    deltaY = distance * direction + skin;
+                }
+                else
+                {
+                    deltaY = 0;
+                }
+
+                grounded = true;
+                break;
+            }
+        }
+
+        var finalTransform = new Vector2(deltaX, deltaY);
+
+        transform.Translate(finalTransform);
+    }
+}
